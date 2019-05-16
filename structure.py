c4688eb3522da7bcb18db8054063d7889329d2b6
aschnurp <anton24697@gmx.de>
2019-05-16 16:31:45 +0200

    updated

--- structure.py
+++ structure.py
@@ -1,6 +1,8 @@
 #cerate icosphere
+import bpy
 bpy.ops.mesh.primitive_ico_sphere_add(subdivisions=2, size=1.0, calc_uvs=False, view_align=False, enter_editmode=False, location=(0.0, 0.0, 0.0), rotation=(0.0, 0.0, 0.0))
 
 #cerate uv sphere
-bpy.ops.mesh.primitive_uv_sphere_add(segments=32, ring_count=16, size=1.0, calc_uvs=False, view_align=False, enter_editmode=False, location=(0.0, 0.0, 0.0), rotation=(0.0, 0.0, 0.0))
+import bpy
+bpy.ops.mesh.primitive_uv_sphere_add(segments=16, ring_count=8, size=1.0, calc_uvs=False, view_align=False, enter_editmode=False, location=(0.0, 0.0, 0.0), rotation=(0.0, 0.0, 0.0))
 
