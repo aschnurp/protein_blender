b6a1e402910c4221661d23f373553b5463793922
aschnurp <anton24697@gmx.de>
2019-05-16 15:56:38 +0200

    create a ico_sphere

---
+++ structure.py
@@ -0,0 +1,2 @@
+#cerate a icosphere
+bpy.ops.mesh.primitive_ico_sphere_add(subdivisions=2, size=1.0, calc_uvs=False, view_align=False, enter_editmode=False, location=(0.0, 0.0, 0.0), rotation=(0.0, 0.0, 0.0))
