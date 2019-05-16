62ab12cbfb05076e4e1bdf970713be565c2d42fb
aschnurp <anton24697@gmx.de>
2019-05-16 16:01:06 +0200

    create a ico_sphere

--- structure.py
+++ structure.py
@@ -1,2 +1,6 @@
 #cerate a icosphere
 bpy.ops.mesh.primitive_ico_sphere_add(subdivisions=2, size=1.0, calc_uvs=False, view_align=False, enter_editmode=False, location=(0.0, 0.0, 0.0), rotation=(0.0, 0.0, 0.0))
+
+#cerate a uv sphere
+bpy.ops.mesh.primitive_uv_sphere_add(segments=32, ring_count=16, size=1.0, calc_uvs=False, view_align=False, enter_editmode=False, location=(0.0, 0.0, 0.0), rotation=(0.0, 0.0, 0.0))
+
